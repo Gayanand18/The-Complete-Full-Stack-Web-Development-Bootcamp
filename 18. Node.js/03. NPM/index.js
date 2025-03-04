a461a9b1eb01b36d0fdca1e485ec92dabfbbfd0d
Gayanand <gayanand.patel@gmail.com>
2025-03-05 05:22:38 +0530

    added files for Node.js demo

---
+++ 18. Node.js/03. NPM/index.js
@@ -0,0 +1,6 @@
+// var generateName = require('sillyname');
+// var sillyName = generateName();
+
+import {randomSuperhero} from 'superheroes';
+var supername = randomSuperhero();
+console.log(`I am ${supername}!`);
